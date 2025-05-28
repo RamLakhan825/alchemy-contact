f3f9ed67c3f0770ed27f9a6e6716456c2938cc16
Ram Lakhan Jangid <rljangid825@gmail.com>
2025-05-28 12:42:53 +0530

    lll

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./pages/hero";
+import Start from "./pages/start";
+import X from "./pages/x";
+import Y from "./pages/y";
+import Z from "./pages/z";
+
+function App() {
+  return (
+    <>
+      <Start></Start>
+      <Hero/>
+      <X/>
+      <Y></Y>
+      <Z/>
+    </>
+  )
+}
+
+export default App
